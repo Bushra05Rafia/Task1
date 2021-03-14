73c2873135d4afd7a3098a21be08eb0dad223a06
Bushra05Rafia <52478317+Bushra05Rafia@users.noreply.github.com>
2021-03-15 03:02:29 +0600

    Collide with Cube under obstacle tag

--- Assets/playercollision.cs
+++ Assets/playercollision.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class playercollision : MonoBehaviour
 {
+    public playermovement movement;
+    void OnCollisionEnter(Collision Collisioninfo)
+    {
+        //if (Collisioninfo.collider.name == "Cube")
+        //{
+        // Debug.Log("Player Collide with Something!");
+        // Debug.Log(Collisioninfo.collider.name);
+        //}
+        if (Collisioninfo.collider.tag == "obstacle")
+        {
+            Debug.Log("Player Collide with Something!");
+            Debug.Log(Collisioninfo.collider.name);
+            //movement.enable = false;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
